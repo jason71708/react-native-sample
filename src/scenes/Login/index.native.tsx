e23d70b376e503fdc93f45834af14a6021ed577b
Jason Zhuang <jason_zhuang@staging.com.tw>
2021-12-21 15:26:47 +0800

    feat: add async-storage plugin

--- src/scenes/Login/index.native.tsx
+++ src/scenes/Login/index.native.tsx
@@ -1,7 +1,14 @@
-import React from 'react';
-import { SafeAreaView, Text, TouchableHighlight } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  TouchableHighlight,
+  TextInput,
+  View,
+} from 'react-native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../navigations';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type ProfileScreenNavigationProp =
   NativeStackNavigationProp<RootStackParamList>;
@@ -10,17 +17,62 @@ const LoginScreen = ({
   navigation,
 }: {
   navigation: ProfileScreenNavigationProp;
-}) => (
-  <SafeAreaView>
-    <Text>Screen: Login</Text>
+}) => {
+  const [username, setUsername] = useState('');
 
-    <TouchableHighlight onPress={() => navigation.navigate('Home')}>
-      <Text>Go to home</Text>
-    </TouchableHighlight>
-    <TouchableHighlight onPress={() => navigation.navigate('Details')}>
-      <Text>Go to Details</Text>
-    </TouchableHighlight>
-  </SafeAreaView>
-);
+  const storeData = async (value: string) => {
+    try {
+      await AsyncStorage.setItem('@username', value);
+    } catch (e) {
+      // saving error
+    }
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('@username');
+        if (value !== null) {
+          setUsername(value);
+        }
+      } catch (e) {
+        // error reading value
+      }
+    };
+    getData();
+  }, []);
+
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Screen: Login</Text>
+      </View>
+      <View>
+        <TextInput
+          onChangeText={setUsername}
+          value={username}
+          onKeyPress={event => {
+            if (event.nativeEvent.key === 'Enter') {
+              storeData(username); // called only when multiline is false
+            }
+          }}
+          onSubmitEditing={() => {
+            storeData(username); //called when multiline is true
+          }}
+        />
+      </View>
+      <View>
+        <TouchableHighlight onPress={() => navigation.navigate('Home')}>
+          <Text>Go to home</Text>
+        </TouchableHighlight>
+      </View>
+      <View>
+        <TouchableHighlight onPress={() => navigation.navigate('Details')}>
+          <Text>Go to Details</Text>
+        </TouchableHighlight>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default LoginScreen;
